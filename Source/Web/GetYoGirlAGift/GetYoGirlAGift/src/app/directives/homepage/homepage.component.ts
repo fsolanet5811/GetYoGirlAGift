7c516e217f85fbcd67d00e8f9ea8ef5182140c2e
muratti.1331@gmail.com <muratti.1331@gmail.com>
2020-04-13 18:37:25 -0400

    updated packages trying to fix app not display, app is still not displaying.

--- Source/Web/GetYoGirlAGift/GetYoGirlAGift/src/app/directives/homepage/homepage.component.ts
+++ Source/Web/GetYoGirlAGift/GetYoGirlAGift/src/app/directives/homepage/homepage.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'Homepage',
-  templateUrl: './directives/homepage/homepage.component.html',
+  templateUrl: './app/directives/homepage/homepage.component.html',
   styleUrls: ['../styles.css']
 })
 export class HomepageComponent implements OnInit {
